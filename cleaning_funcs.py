8109f51fc4b089e29f7c660162750e115115c486
Pietro Palermo <47111609+Pietropa@users.noreply.github.com>
2019-09-12 12:36:49 +0200

    Update cleaning_funcs.py

--- cleaning_funcs.py
+++ cleaning_funcs.py
@@ -1,4 +1,5 @@
 #GENERALIZED FUNCTIONS
+#Data cleaning functions
 pattern = '\-|\@|\€|\,'
 def clean_string(s):
     
@@ -11,7 +12,14 @@ def clean_data(df1,x):
     df1[x]=df1[x].apply(clean_string)
     df1[x] = pd.to_numeric(df1[x], errors='coerce',downcast='signed')
     #df1[x] = df1[x].astype(int)
- 
+    
+def numeric_variables(df,x):
+    variables = list(df[x].unique())
+    variables_count = list(range(len(df[x].unique())))
+    audiences= dict(zip(variables,variables_count))
+    df[f'{x}_n'] = df[x].map(audiences)
+    
+#Data set reshaping 
 #where x needs to be the reference column identifier and m the metric on which the data set needs to be disaggregated, 
 #better to start with visits and the repeating the same process for other metrics
 def index_a(df,x,m): 
@@ -29,7 +37,7 @@ def index_a(df,x,m):
             cycle = range(0, (transition_points[i+1] - transition_points[i]))
 
         except IndexError:
-            cycle = range(0, len(df)-len(cycle_list)) #think of a better way to find this number without hardcoding it!
+            cycle = range(0, len(df)-len(cycle_list)) 
 
         cycle_list += cycle
 
@@ -37,17 +45,5 @@ def index_a(df,x,m):
     df['ID_final']= df[x]+'-'+df['f'].astype(str)
     df[m]= 1
     df.drop(['y','a','f'], axis=1)
-    
-def new_value(df,x,y):
-    new_value=[]
-    for i in df[x]:
-        d = i* y
-        new_value.append(d)
-    return new_value
 
-def numeric_variables(df,x):
-    variables = list(df[x].unique())
-    variables_count = list(range(len(df[x].unique())))
-    audiences= dict(zip(variables,variables_count))
-    df[f'{x}_n'] = df[x].map(audiences)
-    
+
