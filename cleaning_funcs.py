5ad0297f89b0e2cf1a735d1190a2e99446696ba2
Pietro Palermo <47111609+Pietropa@users.noreply.github.com>
2019-09-19 15:41:44 +0200

    Update cleaning_funcs.py

--- cleaning_funcs.py
+++ cleaning_funcs.py
@@ -1,34 +1,40 @@
 #GENERALIZED FUNCTIONS
-#Data cleaning functions
-pattern = '\-|\@|\€|\,'
-def clean_string(s):
+
+#Cleaning functions
+def drop_rename(df,x,y):
     
-    s = re.sub(pattern, '', s)
+    df=df.drop(x,axis=1)
+    df.rename(columns=y,inplace =True)
+    return df
+
+def clean_string(x):
+    import re
+    
+    s = re.sub(pattern, '', x)
     return s.lower()
 
-def clean_data(df1,x):
+def clean_data(df,x):
+    import pandas as pd
 
-    df1[x]= df1[x].astype('str')
-    df1[x]=df1[x].apply(clean_string)
-    df1[x] = pd.to_numeric(df1[x], errors='coerce',downcast='signed')
-    #df1[x] = df1[x].astype(int)
-    
+    df[x]= df[x].astype('str')
+    df[x] = pd.to_numeric(df[x], errors='coerce',downcast='signed')
+
 def numeric_variables(df,x):
     variables = list(df[x].unique())
     variables_count = list(range(len(df[x].unique())))
     audiences= dict(zip(variables,variables_count))
     df[f'{x}_n'] = df[x].map(audiences)
+     
+ #Dataframe transformation functions  
+def unique_ID(df,uniqueID_list):
+    df['uniqueID'] = df[uniqueID_list].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
     
-#Data set reshaping 
 
-#Create new dataframe based on a metric
 def new_df_db(df,x):
-    y = df.loc[df.index.repeat(df[x])]
+    y = df.loc[df.index.repeat(df[x])] 
     return y
 
-#where x needs to be the reference column identifier and m the metric on which the data set needs to be disaggregated, 
-#better to start with visits and the repeating the same process for other metrics
-def index_a(df,x,m): 
+def index_a(df,x,m):
     df['y'] = df[x].shift(1)
 
     df['a'] = (df['y'] != df[x]).astype(int)
@@ -43,7 +49,7 @@ def index_a(df,x,m):
             cycle = range(0, (transition_points[i+1] - transition_points[i]))
 
         except IndexError:
-            cycle = range(0, len(df)-len(cycle_list)) 
+            cycle = range(0, len(df)-len(cycle_list)) #think of a better way to find this number without hardcoding it!
 
         cycle_list += cycle
 
@@ -51,5 +57,20 @@ def index_a(df,x,m):
     df['ID_final']= df[x]+'-'+df['f'].astype(str)
     df[m]= 1
     df.drop(['y','a','f'], axis=1)
+
+
+# extra functions   
+def new_value(df,x,y):
+    new_value=[]
+    for i in df[x]:
+        d = i* y
+        new_value.append(d)
+    return new_value
+
+def date_extractor(df,x):
+    import datetime as dt
+    df['month'] = df[x].dt.month
+    df['year']= df[x].dt.year
+    df['day_of_week'] = df[x].dt.weekday_name
 
 
